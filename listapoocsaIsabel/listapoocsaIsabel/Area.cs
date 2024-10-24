b8afff2540f07a75d2f803fee854edaf87b5ded9
paixaoisabel <167146259+paixaoisabel@users.noreply.github.com>
2024-10-24 20:12:28 -0300

    Add files via upload

---
+++ listapoocsaIsabel/listapoocsaIsabel/Area.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listapoocsaIsabel
+{
+    internal class Area
+    {
+        private int a;
+        private int b;
+        private int area;
+
+        public void setaltura(int a)
+        {
+            this.a = a;
+        }
+
+        public void setbase(int b)
+        {
+            this.b = b;
+        }
+
+        public int getaltura()
+        {
+            return this.a;
+        }
+
+        public int getbase()
+        {
+            return this.b;
+        }
+
+        public int getarea()
+        {
+            return this.area;
+        }
+
+        public void calcularArea()
+        {
+            this.area = this.b * this.a;
+        }
+    }
+}
---
+++ listapoocsaIsabel/listapoocsaIsabel/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listapoocsaIsabel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Area ar;
+            ar = new Area();
+
+            Console.Write("Digite o valor da base: ");
+            ar.setbase(int.Parse(Console.ReadLine()));
+
+            Console.Write("Digite o valor da altura: ");
+            ar.setaltura(int.Parse(Console.ReadLine()));
+
+            ar.calcularArea();
+
+            Console.WriteLine("O valor da area é de: {0}",ar.getarea());
+
+        }
+    }
+}
